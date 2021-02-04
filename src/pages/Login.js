bf063123c15aeb2873cf1e9452bcba5312ee2063
kaikykeavyday1 <pongpipat.tawatchaidumrong@mail.kmutt.ac.th>
2021-02-04 15:54:09 +0700

    edit login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,20 @@
 import React from 'react'
-
+import logo from '../images/logo.png'
 export default function Login() {
     return (
-        <div className = "container">
+        <div className = "container mt-5">
            <div className ="col-6 mx-auto">
-                    <h1 className="text-center">Sign in</h1>
+               <img src = {logo} alt = "img4" height="70px" className="justify-content-center"/>
+                <h1>เข้าสู่ระบบ</h1>
                     <form>
                         <div className = "form-group">
-                            <label htmlfor="Inputusername">dokmaiban55</label>
-                            <input type = "text" name = "username" className = "form-control"/>
+                            <input type = "text" name = "username" className = "form-control" placeholder= "อีเมล"/>
                         </div>
                         <div>
-                            <label htmlfor="Inputpassword">password</label>
-                            <input type = "password" name = "password" className = "form-control"/>
+                            <input type = "password" name = "password" className = "form-control" placeholder= "รหัสผ่าน"/>
                         </div>
                         <div className = "mt-3 text-center">
-                            <button type = "submit" className = "btn-lg btn-primary">Sign in</button>
+                            <button type = "submit" className = "btn-lg btn-primary">เข้าสู่ระบบ</button>
                         </div>
                     </form>
                     </div>
