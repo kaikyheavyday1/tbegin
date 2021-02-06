e5a0f859f5be21fff11b1afd6fb602850783f0b5
kaikykeavyday1 <pongpipat.tawatchaidumrong@mail.kmutt.ac.th>
2021-02-07 01:01:18 +0700

    edit access-token

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React , { useState } from 'react'
 import logo from '../images/logo.png'
 import axios from "axios"
 
+let initState = {
+  email : null,
+  password : null
+}
 
 export default function Login() {
-    const ButtonOnclick = async() =>{
-        const fetch = await axios.get("http://localhost:4000")
+
+    const [user, setUser] = useState(initState)
+
+    const handleLoginChange = (e) =>{
+        const id = e.target.id
+        const value = e.target.value
+        setUser({ ...user, [id]: value })
+    }
+    
+    const ButtonOnclick = async(e) =>{
+        
+       if(user.email !== "" && user.password !== ""){
+        const fetch = await axios.post("http://localhost:4000/auth/login", user)
         const data = await fetch.data
         console.log(data)
+        e.preventDefault()
+        localStorage.setItem('access-token', data);
+       } 
+       
     }
 
     return (
@@ -16,10 +35,10 @@ export default function Login() {
                <img src = {logo} alt = "img4" height="70px" className="justify-content-center"/>
                 <h1>เข้าสู่ระบบ</h1>
                         <div className = "form-group">
-                            <input type = "email" name = "email" className = "form-control" placeholder= "อีเมล"/>
+                            <input type = "email" name = "email" id ="email" onChange = {handleLoginChange} className = "form-control" placeholder= "อีเมล"/>
                         </div>
                         <div>
-                            <input type = "password" name = "password" className = "form-control" placeholder= "รหัสผ่าน"/>
+                            <input type = "password" name = "password" id = "password" onChange = {handleLoginChange}  className = "form-control" placeholder= "รหัสผ่าน"/>
                         </div>
                         <div className = "mt-3 text-center">
                             <button type = "button" onClick={ButtonOnclick} className = "btn-lg btn-primary">เข้าสู่ระบบ</button>
