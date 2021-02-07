5d8718aded1389fde03d9b549d45124d134b6433
ninjapooh1 <paphawinya.t@mail.kmutt.ac.th>
2021-02-08 03:24:23 +0700

    เหลือ contact

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Howto from './pages/Howto.js'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './component/Home'
+import Aboutus from './pages/Aboutus'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path = "/howto" component ={Howto}/>
         <Route path = "/login" component ={Login}/>
         <Route path = "/register" component ={Register}/>
+        <Route path = "/Aboutus" component ={Aboutus}/>
     </div>
   );
 }
