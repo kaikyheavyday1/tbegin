697ddf26d27537e9aca329e64f998e9ccbe3b3e2
kaikykeavyday1 <pongpipat.tawatchaidumrong@mail.kmutt.ac.th>
2021-02-08 02:18:34 +0700

    new nav after login success

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import logo from "../images/logo in navbar.png"
+import user from "../images/user.svg"
 import { withRouter, Link } from 'react-router-dom'
 import {
   Collapse,
@@ -8,6 +9,7 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
+  Dropdown, DropdownItem, DropdownToggle, DropdownMenu,
   NavLink,
 } from 'reactstrap';
 
@@ -16,10 +18,12 @@ class Header extends Component {
     super(props);
 
     this.state = {
-      isOpen: false
+      isOpen: false,
+      dropdownOpen: true
     };
 
     this.toggle = this.toggle.bind(this);
+    this.dropdowntoggle = this.dropdowntoggle.bind(this);
   }
 
   toggle() {
@@ -27,6 +31,13 @@ class Header extends Component {
       isOpen: !this.state.isOpen
     });
   }
+
+  dropdowntoggle(){
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
   render() {
     return (
       <div>
@@ -51,8 +62,19 @@ class Header extends Component {
         <Nav className="allnav-item ml-auto" navbar>
           <NavLink><Link to="/hiring">จ้างงาน</Link></NavLink>
           <NavLink><Link to="/uploadwork">ลงผลงาน</Link></NavLink>
+          <NavLink><Link to="/aboutus">เกี่ยวกับเรา</Link></NavLink>
           <NavLink><Link to="/howto">วิธีการใช้งาน</Link></NavLink>
-          <NavLink><a href ='' onClick = {this.Logout}>ออกจากระบบ</a></NavLink>
+          <NavLink><Link to="/">สมัครเป็นฟรีแลนซ์</Link></NavLink>
+          <Dropdown className ="ml-2" isOpen={this.state.dropdownOpen} toggle={this.dropdowntoggle}>
+            <DropdownToggle className="user-dropdown">
+              <img src = {user} alt = "user" height ="35px"/>
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>โปรไฟล์ของฉัน</DropdownItem>
+              <DropdownItem>ข้อความ</DropdownItem>
+              <DropdownItem><a href='' onClick={this.Logout}>ออกจากระบบ</a></DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </Nav>
       )
     } else {
@@ -60,6 +82,7 @@ class Header extends Component {
         <Nav className="allnav-item ml-auto" navbar>
           <NavLink><Link to="/hiring">จ้างงาน</Link></NavLink>
           <NavLink><Link to="/uploadwork">ลงผลงาน</Link></NavLink>
+          <NavLink><Link to="/aboutus">เกี่ยวกับเรา</Link></NavLink>
           <NavLink><Link to="/howto">วิธีการใช้งาน</Link></NavLink>
           <NavLink><Link to="/login">เข้าสู่ระบบ</Link></NavLink>
           <NavLink id="btn-nav"><Link to="/register">สมัครเป็นฟรีแลนซ์</Link></NavLink>
@@ -67,7 +90,7 @@ class Header extends Component {
       )
     }
   }
-  Logout = (e) =>{
+  Logout = (e) => {
     e.preventDefault()
     localStorage.removeItem('access-token')
     window.location.href = "http://localhost:3000/"
