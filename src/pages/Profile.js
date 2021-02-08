e78327ec63930be542c032810ff03dc122cf2a1f
kaikykeavyday1 <pongpipat.tawatchaidumrong@mail.kmutt.ac.th>
2021-02-09 00:37:54 +0700

    profile frontend success

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Row, Col } from 'reactstrap';
+import {
+    Row, Col,
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 import user from "../images/user.svg"
 import graduate from "../images/profile/graduation-hat.svg"
 import letter from "../images/profile/envelope.svg"
 import phone from "../images/profile/phone-call.svg"
+import web from "../images/profile/web.jpeg"
+import web2 from "../images/profile/web2.png"
+
+
 
 export default function Profile() {
     return (
         <div className="profile">
             <div className="container">
                 <Row className="mt-5">
-                    <Col lg="4" md="12" sm="12" xs="12">
+                    <Col lg="4" md="12" sm="12" xs="12" className="profile-info">
                         <div>
-                            <Row>
+                            <Row className="mt-5">
                                 <Col lg="4">
                                     <img src={user} alt="user" height="70px" width="100%" />
                                 </Col>
@@ -25,8 +33,8 @@ export default function Profile() {
                         </div>
                         <div>
                             <div className="d-flex justify-content-around mt-3">
-                                <button type="button">ตั้งค่าโปรไฟล์</button>
-                                <button type="button">ส่งข่้อความ</button>
+                                <button type="button" className="btn-profile">ตั้งค่าโปรไฟล์</button>
+                                <button type="button" className="btn-profile">ส่งข่้อความ</button>
                             </div>
                             <div className="mt-3">
                                 <p>ป๋าใจดีสปอร์ต กทม</p>
@@ -50,38 +58,82 @@ export default function Profile() {
                             </div>
                             <div className="mt-3">
                                 <h3>Contact</h3>
-                                <div className="d-flex justify-content-start">                                    
-                                    <div><img src={phone} alt="user" height="20px"/></div>                                    
+                                <div className="d-flex justify-content-start">
+                                    <div><img src={phone} alt="user" height="20px" /></div>
                                     <div className="ml-2"><h5>0807854451</h5></div>
                                 </div>
-                                <div className="d-flex justify-content-start">                                    
-                                    <div><img src={letter} alt="user" height="20px"/></div>                                    
+                                <div className="d-flex justify-content-start">
+                                    <div><img src={letter} alt="user" height="20px" /></div>
                                     <div className="ml-2"><h5>rabite02013@hotmail.com</h5></div>
                                 </div>
-                                <div className="d-flex justify-content-start">                                    
-                                    <div><img src={graduate} alt="user" height="20px"/></div>                                    
+                                <div className="d-flex justify-content-start">
+                                    <div><img src={graduate} alt="user" height="20px" /></div>
                                     <div className="ml-2"><h5>โรงเรียนบางปะกอกวิทยาคม</h5></div>
                                 </div>
                             </div>
                             <div>
                                 <h3>ความถนัด</h3>
                                 <div className="d-flex justify-content-around mt-3">
-                                <div className="">
-                                    <h5>ผลงาน</h5>
+                                    <div className="profess">
+                                        <h5>สอนพิเศษ</h5>
+                                    </div>
+                                    <div className="profess">
+                                        <h5>การคลาด</h5>
+                                    </div>
+                                    <div className="profess">
+                                        <h5>ถ่ายภาพ</h5>
+                                    </div>
                                 </div>
-                                <div className="">
-                                    <h5>เรทติ้ง</h5>
-                                </div>
-                                <div className="">
-                                    <h5>สำเร็จ</h5>
-                                </div>
-                            </div>
                             </div>
                         </div>
                     </Col>
-                    <Col lg="8" md="12" sm="12" xs="12">
+                    <Col lg="8" md="12" sm="12" xs="12" className="mt-5">
                         <h1 className="text-center">โปรไฟล์ของคุณ</h1>
                         <h4 className="text-center mt-3">ผลงาน</h4>
+                        <div className="tester">
+                            <Row className="testing">
+                                <Col lg="6" md="6" sm="12" xs="12" className="mt-3">
+                                    <Card>
+                                        <CardImg top width="80%" src={web2} alt="Card image cap" />
+                                        <CardBody>
+                                            <CardTitle tag="h3">ออกแบบเว็บไซต์ tbegin</CardTitle>
+                                            <CardSubtitle tag="h5" className="text-right mb-2 text-muted">Freelance : kaikyheavyday</CardSubtitle>
+                                            <CardText className="text-left">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col lg="6" md="6" sm="12" xs="12" className="mt-3">
+                                    <Card>
+                                        <CardImg top width="80%" src={web} alt="Card image cap" />
+                                        <CardBody>
+                                            <CardTitle tag="h3">ออกแบบเว็บไซต์ tbegin</CardTitle>
+                                            <CardSubtitle tag="h5" className="text-right mb-2 text-muted">ฺFreelance : kaikyheavyday</CardSubtitle>
+                                            <CardText className="text-left">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col lg="6" md="6" sm="12" xs="12" className="mt-3">
+                                    <Card>
+                                        <CardImg top width="80%" src={web} alt="Card image cap" />
+                                        <CardBody>
+                                            <CardTitle tag="h3">ออกแบบเว็บไซต์ tbegin</CardTitle>
+                                            <CardSubtitle tag="h5" className="text-right mb-2 text-muted">ฺFreelance : kaikyheavyday</CardSubtitle>
+                                            <CardText className="text-left">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col lg="6" md="6" sm="12" xs="12" className="mt-3">
+                                    <Card>
+                                        <CardImg top width="80%" src={web2} alt="Card image cap" />
+                                        <CardBody>
+                                            <CardTitle tag="h3">ออกแบบเว็บไซต์ tbegin</CardTitle>
+                                            <CardSubtitle tag="h5" className="text-right mb-2 text-muted">Freelance : kaikyheavyday</CardSubtitle>
+                                            <CardText className="text-left">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
                     </Col>
                 </Row>
             </div>
