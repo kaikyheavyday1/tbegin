21e1f1cd5fadfae563e3cd3241beb96c23f74072
kaikykeavyday1 <pongpipat.tawatchaidumrong@mail.kmutt.ac.th>
2021-02-13 04:13:38 +0700

    add modal working

--- src/pages/Working.js
+++ src/pages/Working.js
@@ -4,7 +4,8 @@ import {
     CarouselItem,
     CarouselControl,
     CarouselIndicators,
-    CarouselCaption, Row, Col
+    CarouselCaption, Row, Col,
+    Modal, ModalHeader, ModalBody, ModalFooter
 } from 'reactstrap';
 import { Link } from 'react-router-dom'
 
@@ -27,6 +28,19 @@ const items = [
 export default function Working() {
     const [activeIndex, setActiveIndex] = useState(0);
     const [animating, setAnimating] = useState(false);
+    const [modal, setModal] = useState(false);
+    const [modal2, setModal2] = useState(false);
+    const [modal3, setModal3] = useState(false);
+    const [modal4, setModal4] = useState(false);
+    const [modal5, setModal5] = useState(false);
+    const [modal6, setModal6] = useState(false);
+
+    const toggle = () => setModal(!modal);
+    const toggle2 = () => setModal2(!modal2);
+    const toggle3 = () => setModal3(!modal3);
+    const toggle4 = () => setModal4(!modal4);
+    const toggle5 = () => setModal5(!modal5);
+    const toggle6 = () => setModal6(!modal6);
 
     const next = () => {
         if (animating) return;
@@ -71,25 +85,43 @@ export default function Working() {
                         </Carousel>
                         <Row className="working-small mt-2">
                             <Col lg="2" md="2" sm="4" xs="4" className="img-working-small">
-                                <img src={web} alt="web" className="w-100" height="100%"></img>
+                                <img src={web} alt="web" className="w-100" height="100%" onClick={toggle}></img>
+                                <Modal isOpen={modal} toggle={toggle} >                                  
+                                    <img src={web} alt="web" className="w-100" height="100%"></img>                    
+                                </Modal>
                             </Col>
                             <Col lg="2" md="2" sm="4" xs="4" className="img-working-small">
-                                <img src={web2} alt="web" className="w-100" height="75px"></img>
+                                <img src={web2} alt="web" className="w-100" height="75px" onClick={toggle2}></img>
+                                <Modal isOpen={modal2} toggle={toggle2} >                                  
+                                    <img src={web2} alt="web" className="w-100" height="100%"></img>                    
+                                </Modal>
                             </Col>
                             <Col lg="2" md="2" sm="4" xs="4" className="img-working-small">
-                                <img src={web3} alt="web" className="w-100" height="75px"></img>
+                                <img src={web3} alt="web" className="w-100" height="75px" onClick={toggle3}></img>
+                                <Modal isOpen={modal3} toggle={toggle3} >                                  
+                                    <img src={web3} alt="web" className="w-100" height="100%"></img>                    
+                                </Modal>
                             </Col>
                             <Col lg="2" md="2" sm="4" xs="4" className="img-working-small">
-                                <img src={web4} alt="web" className="w-100" height="75px"></img>
+                                <img src={web4} alt="web" className="w-100" height="75px" onClick={toggle4}></img>
+                                <Modal isOpen={modal4} toggle={toggle4} >                                  
+                                    <img src={web4} alt="web" className="w-100" height="100%"></img>                    
+                                </Modal>
                             </Col>
                             <Col lg="2" md="2" sm="4" xs="4" className="img-working-small">
-                                <img src={web5} alt="web" className="w-100" height="75px"></img>
+                                <img src={web5} alt="web" className="w-100" height="75px" onClick={toggle5}></img>
+                                <Modal isOpen={modal5} toggle={toggle5} >                                  
+                                    <img src={web5} alt="web" className="w-100" height="100%"></img>                    
+                                </Modal>
                             </Col>
                             <Col lg="2" md="2" sm="4" xs="4" className="img-working-small">
-                                <img src={web6} alt="web" className="w-100" height="75px"></img>
+                                <img src={web6} alt="web" className="w-100" height="75px" onClick={toggle6}></img>
+                                <Modal isOpen={modal6} toggle={toggle6} >                                  
+                                    <img src={web6} alt="web" className="w-100" height="100%"></img>                    
+                                </Modal>
                             </Col>
                         </Row>
-                        <div className = "mt-5">
+                        <div className="mt-5">
                             <h3>รายละเอียดของงาน</h3>
                             <p>ออกแบบโลโก้ให้ 2 แบบ เลือกได้ 1 แบบ แก้ไขได้ 2 ครั้ง ส่งเป็นไฟล์ AI หรือไฟล์อื่นๆที่ลูกค้าต้องการ</p>
                             <h3>สิ่งที่ลูกค้าจะได้รับ</h3>
@@ -108,59 +140,59 @@ export default function Working() {
                     </Col>
                     <Col lg="4" className="mt-2">
                         <div className="working-right col-10 mx-auto">
-                        <div>
-                            <div className="d-flex pt-3">
-                                <img src ={userpic} alt ="userpic"></img>
-                                <h4 className ="pt-3 ml-2">พงศ์พิพัฒน์ ธวัชชัยดำรงค์</h4>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="mt-3">
-                                <p>ป๋าใจดีสปอร์ต กทม</p>
-                            </div>
-                            <div className="mt-3">
-                                <h5>สมัครสมาชิกเมื่อ 18/02/2564</h5>
-                            </div>
-                            <div className="d-flex justify-content-around mt-3">
-                                <div className="text-center">
-                                    <h3>0</h3>
-                                    <h5>ผลงาน</h5>
-                                </div>
-                                <div className="text-center">
-                                    <h3>0</h3>
-                                    <h5>เรทติ้ง</h5>
-                                </div>
-                                <div className="text-center">
-                                    <h3>0</h3>
-                                    <h5>สำเร็จ</h5>
-                                </div>
-                            </div>
-                            <div className="mt-3">
-                                <h3>Contact</h3>
-                                <div className="d-flex justify-content-start">
-                                    <div><img src={phone} alt="user" height="20px" /></div>
-                                    <div className="ml-2"><h5>0807854451</h5></div>
-                                </div>
-                                <div className="d-flex justify-content-start">
-                                    <div><img src={letter} alt="user" height="20px" /></div>
-                                    <div className="ml-2"><h5>rabite02013@hotmail.com</h5></div>
-                                </div>
-                                <div className="d-flex justify-content-start">
-                                    <div><img src={graduate} alt="user" height="20px" /></div>
-                                    <div className="ml-2"><h5>โรงเรียนบางปะกอกวิทยาคม</h5></div>
+                            <div>
+                                <div className="d-flex pt-3">
+                                    <img src={userpic} alt="userpic"></img>
+                                    <h4 className="pt-3 ml-2">พงศ์พิพัฒน์ ธวัชชัยดำรงค์</h4>
                                 </div>
                             </div>
                             <div>
-                                <h3 className="mt-3">ความถนัด</h3>
+                                <div className="mt-3">
+                                    <p>ป๋าใจดีสปอร์ต กทม</p>
+                                </div>
+                                <div className="mt-3">
+                                    <h5>สมัครสมาชิกเมื่อ 18/02/2564</h5>
+                                </div>
+                                <div className="d-flex justify-content-around mt-3">
+                                    <div className="text-center">
+                                        <h3>0</h3>
+                                        <h5>ผลงาน</h5>
+                                    </div>
+                                    <div className="text-center">
+                                        <h3>0</h3>
+                                        <h5>เรทติ้ง</h5>
+                                    </div>
+                                    <div className="text-center">
+                                        <h3>0</h3>
+                                        <h5>สำเร็จ</h5>
+                                    </div>
+                                </div>
+                                <div className="mt-3">
+                                    <h3>Contact</h3>
+                                    <div className="d-flex justify-content-start">
+                                        <div><img src={phone} alt="user" height="20px" /></div>
+                                        <div className="ml-2"><h5>0807854451</h5></div>
+                                    </div>
+                                    <div className="d-flex justify-content-start">
+                                        <div><img src={letter} alt="user" height="20px" /></div>
+                                        <div className="ml-2"><h5>rabite02013@hotmail.com</h5></div>
+                                    </div>
+                                    <div className="d-flex justify-content-start">
+                                        <div><img src={graduate} alt="user" height="20px" /></div>
+                                        <div className="ml-2"><h5>โรงเรียนบางปะกอกวิทยาคม</h5></div>
+                                    </div>
+                                </div>
                                 <div>
-                                   <ul>
-                                       <li><p>สอนพิเศษ</p></li>
-                                       <li><p>ออกแบบ</p></li>
-                                       <li><p>กราฟฟิก</p></li>
-                                   </ul>
+                                    <h3 className="mt-3">ความถนัด</h3>
+                                    <div>
+                                        <ul>
+                                            <li><p>สอนพิเศษ</p></li>
+                                            <li><p>ออกแบบ</p></li>
+                                            <li><p>กราฟฟิก</p></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         </div>
                     </Col>
                 </Row>
