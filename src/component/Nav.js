6607b1fbf9ffb7ac1b807ec320cd6565865640e4
kaikykeavyday1 <pongpipat.tawatchaidumrong@mail.kmutt.ac.th>
2021-02-26 02:06:06 +0700

    add success merge

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import logo from "../images/logo in navbar.png";
-import user from "../images/nav/user.png";
-import noti from "../images/nav/bell.png";
-import chat from "../images/nav/chat.png";
-import { withRouter, Link } from "react-router-dom";
-=======
 import React, { Component } from 'react'
 import logo from '../images/logo in navbar.png'
-import user from '../images/user.svg'
+import user from '../images/nav/user.png'
+import noti from '../images/nav/bell.png'
+import chat from '../images/nav/chat.png'
 import { withRouter, Link } from 'react-router-dom'
->>>>>>> mp/hiring
 import {
   Collapse,
   Navbar,
@@ -23,11 +16,7 @@ import {
   DropdownToggle,
   DropdownMenu,
   NavLink,
-<<<<<<< HEAD
-} from "reactstrap";
-=======
 } from 'reactstrap'
->>>>>>> mp/hiring
 
 class Header extends Component {
   constructor(props) {
@@ -36,11 +25,7 @@ class Header extends Component {
     this.state = {
       isOpen: false,
       dropdownOpen: true,
-<<<<<<< HEAD
-    };
-=======
     }
->>>>>>> mp/hiring
 
     this.toggle = this.toggle.bind(this)
     this.dropdowntoggle = this.dropdowntoggle.bind(this)
@@ -49,21 +34,13 @@ class Header extends Component {
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen,
-<<<<<<< HEAD
-    });
-=======
     })
->>>>>>> mp/hiring
   }
 
   dropdowntoggle() {
     this.setState((prevState) => ({
       dropdownOpen: !prevState.dropdownOpen,
-<<<<<<< HEAD
-    }));
-=======
     }))
->>>>>>> mp/hiring
   }
 
   render() {
@@ -81,22 +58,15 @@ class Header extends Component {
           </div>
         </Navbar>
       </div>
-    );
+    )
   }
 
   renderNavlink = () => {
-    if (localStorage.getItem("access-token")) {
+    if (localStorage.getItem('access-token')) {
       return (
         <Nav className="allnav-item ml-auto" navbar>
           <NavLink>
-<<<<<<< HEAD
             <Link to="/hiring">จ้างงาน</Link>
-=======
-            <Link to="/sidebar">จ้างงาน</Link>
-          </NavLink>
-          <NavLink>
-            <Link to="/uploadwork">ลงผลงาน</Link>
->>>>>>> mp/hiring
           </NavLink>
           <NavLink>
             <Link to="/aboutus">เกี่ยวกับเรา</Link>
@@ -104,26 +74,19 @@ class Header extends Component {
           <NavLink>
             <Link to="/howto">วิธีการใช้งาน</Link>
           </NavLink>
-<<<<<<< HEAD
           <NavLink className="nav-link-img">
             <img src={chat} alt="noti" height="25px" />
           </NavLink>
           <NavLink className="nav-link-img">
             <img src={noti} alt="noti" height="25px" />
           </NavLink>
-=======
->>>>>>> mp/hiring
           <Dropdown
             className="ml-2"
             isOpen={this.state.dropdownOpen}
             toggle={this.dropdowntoggle}
           >
             <DropdownToggle className="user-dropdown">
-<<<<<<< HEAD
               <img src={user} alt="user" height="30px" />
-=======
-              <img src={user} alt="user" height="35px" />
->>>>>>> mp/hiring
             </DropdownToggle>
             <DropdownMenu>
               <DropdownItem>
@@ -132,12 +95,9 @@ class Header extends Component {
               <DropdownItem>
                 <Link to="/registerfreelance">สมัครเป็นฟรีแลนซ์</Link>
               </DropdownItem>
-<<<<<<< HEAD
               <DropdownItem>
                 <Link to="/uploadwork">ลงผลงาน</Link>
               </DropdownItem>
-=======
->>>>>>> mp/hiring
               <DropdownItem>ข้อความ</DropdownItem>
               <DropdownItem>
                 <a href="" onClick={this.Logout}>
@@ -147,16 +107,12 @@ class Header extends Component {
             </DropdownMenu>
           </Dropdown>
         </Nav>
-      );
+      )
     } else {
       return (
         <Nav className="allnav-item ml-auto" navbar>
           <NavLink>
-<<<<<<< HEAD
             <Link to="/hiring">จ้างงาน</Link>
-=======
-            <Link to="/sidebar">จ้างงาน</Link>
->>>>>>> mp/hiring
           </NavLink>
           <NavLink>
             <Link to="/uploadwork">ลงผลงาน</Link>
@@ -174,20 +130,13 @@ class Header extends Component {
             <Link to="/register">สมัครสมาชิก</Link>
           </NavLink>
         </Nav>
-      );
+      )
     }
-  };
+  }
   Logout = (e) => {
-<<<<<<< HEAD
-    e.preventDefault();
-    localStorage.removeItem("access-token");
-    window.location.href = "http://localhost:3000/";
-  };
-=======
     e.preventDefault()
     localStorage.removeItem('access-token')
     window.location.href = 'http://localhost:3000/'
   }
->>>>>>> mp/hiring
 }
-export default withRouter(Header);
+export default withRouter(Header)
