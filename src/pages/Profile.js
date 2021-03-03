5f317478935eb630df14d2ded8ee9e0e31c19f52
ninjapooh1 <paphawinya.t@mail.kmutt.ac.th>
2021-03-04 01:55:26 +0700

    no message

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -164,7 +164,7 @@ export default function Profile() {
                           key={index}
                         >
                           <Card className="cardprofilework">
-                            <h1 className="ml-3 mt-3">{work.name}</h1>
+                            <h1 className="ml-3 mt-3">ชื่อผลงาน {work.name}</h1>
 
                             <CardBody>
                               <CardImg
